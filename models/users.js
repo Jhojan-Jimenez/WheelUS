9ffb5164ae713e0510536c9b2f0ba50dd122b0a0
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-11-07 19:56:52 -0500

    Arreglo nulls en getVehicleRides

--- models/users.js
+++ models/users.js
@@ -77,14 +77,12 @@ class usersModel {
   static async getUserRides(id) {
     const user = await this.getUserById(id);
     const ridesInfo = user.rides
-      ? (
-          await Promise.all(
-            user.rides.map(async ({ rideId, arrivalPoints }) => {
-              const rideData = await ridesModel.getRideById(rideId);
-              return rideData.isActive ? { rideId, ...rideData } : undefined;
-            })
-          )
-        ).filter(Boolean)
+      ? await Promise.all(
+          user.rides.map(async ({ rideId, arrivalPoints }) => {
+            const rideData = await ridesModel.getRideById(rideId);
+            return rideData.isActive ? { rideId, ...rideData } : null;
+          })
+        ).then((rides) => rides.filter(Boolean))
       : [];
 
     return ridesInfo;
